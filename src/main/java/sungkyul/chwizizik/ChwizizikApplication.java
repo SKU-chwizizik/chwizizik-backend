620839bff5cc50f3614d94f22fb5ddd72f3e86b9
강민성 <kang0312-@naver.com>
2026-02-12 23:00:35 +0900

    데베 연동

--- src/main/java/sungkyul/chwizizik/ChwizizikApplication.java
+++ src/main/java/sungkyul/chwizizik/ChwizizikApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // db없이 실행
+@SpringBootApplication
 public class ChwizizikApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChwizizikApplication.class, args);
