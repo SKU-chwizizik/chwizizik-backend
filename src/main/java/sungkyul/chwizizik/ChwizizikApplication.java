b5cef6ceb9c45de959fb71248d929e029a66234a
동화 <donghwa0702@gmail.com>
2026-02-23 21:01:41 +0900

    환경 변수 수정

--- src/main/java/sungkyul/chwizizik/ChwizizikApplication.java
+++ src/main/java/sungkyul/chwizizik/ChwizizikApplication.java
@@ -5,10 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ChwizizikApplication {
 	public static void main(String[] args) {
+
+	// .env 파일 로드
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    
+    // .env에 있는 모든 변수를 시스템 프로퍼티로 등록
+    dotenv.entries().forEach(entry -> {
+        System.setProperty(entry.getKey(), entry.getValue());
+    });
+
 		SpringApplication.run(ChwizizikApplication.class, args);
 	}
 }
